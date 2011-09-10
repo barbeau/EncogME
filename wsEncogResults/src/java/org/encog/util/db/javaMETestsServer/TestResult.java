b5a8d6b0f67c8500c0b524681bfbd0c6fe68f4bb
barbeau <sjbarbeau@gmail.com>
2011-09-10 02:49:56 -0300

    Added license and class description info.

--- wsEncogResults/src/java/org/encog/util/db/javaMETestsServer/TestResult.java
+++ wsEncogResults/src/java/org/encog/util/db/javaMETestsServer/TestResult.java
@@ -1,13 +1,26 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
+/**
+Copyright 2010 Sean Barbeau
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+**/
 
 package org.encog.util.db.javaMETestsServer;
 
 /**
- *
- * @author Sean
+ * This is a container class to hold test results from the neural network
+ * @author Sean Barbeau
  */
 public class TestResult {
 
