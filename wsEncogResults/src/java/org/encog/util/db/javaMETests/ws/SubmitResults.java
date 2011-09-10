878800bf8402924a289a2601987a686c42ffa2a5
barbeau <sjbarbeau@gmail.com>
2011-09-10 02:48:01 -0300

    Added license and class description info

--- wsEncogResults/src/java/org/encog/util/db/javaMETests/ws/SubmitResults.java
+++ wsEncogResults/src/java/org/encog/util/db/javaMETests/ws/SubmitResults.java
@@ -1,7 +1,20 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
+/**
+Copyright 2010 Sean Barbeau
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+**/
 
 package org.encog.util.db.javaMETests.ws;
 
@@ -18,8 +31,9 @@ import org.encog.util.db.javaMETestsServer.TestResult;
 import org.encog.util.db.javaMETestsServer.TestResultManager;
 
 /**
- *
- * @author Sean
+ * This class defines the web methods that are accessed from the mobile client to retrieve the neural network model
+ * 
+ * @author Sean Barbeau
  */
 @WebService()
 public class SubmitResults {
